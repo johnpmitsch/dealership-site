34fd04ccc2db7fcb0a51d4e170a79c70c4cfc0e7
johnpmitsch <johnpmitsch@gmail.com>
2015-04-30 11:42:46 -0400

    added meta viewport settings for mobile viewing

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,15 @@
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region                 => 'us-east-1' # Change this for different AWS region. Default is 'us-east-1'
-  }
-  config.cache_dir = "#{Rails.root}/tmp/uploads"
-  config.fog_directory  = ENV['S3_BUCKET']
+if Rails.env.development? || Rails.env.test?
+  puts "development"
+else
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+        :provider               => 'AWS',
+        :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+        :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+        :region                 => 'us-east-1' # Change this for different AWS region. Default is 'us-east-1'
+    }
+    config.cache_dir = "#{Rails.root}/tmp/uploads"
+    config.fog_directory  = ENV['S3_BUCKET']
+  end
 end
